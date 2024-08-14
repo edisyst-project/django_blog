3f38ea6499e19d68a15069c8cf89cc6ec37f5b35
Edoardo <editestar@gmail.com>
2024-08-14 19:34:08 +0200

    Aggiunte funzionalità di modifica ed eliminazione post, miglioramenti grafici con Bootstrap

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='blog-home'),
     path('about/', views.about, name='blog-about'),
-    path('post/<int:pk>/', views.post_detail, name='post-detail'),  # Nuova rotta per il dettaglio
+    path('post/<int:pk>/', views.post_detail, name='post-detail'),         # Nuova rotta per il dettaglio
+    path('post/<int:pk>/update/', views.post_update, name='post-update'),  # Nuova rotta per la modifica
+    path('post/<int:pk>/delete/', views.post_delete, name='post-delete'),  # Nuova rotta per l'eliminazione
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.shortcuts import redirect
 
 def home(request):
     posts = Post.objects.all()
@@ -12,3 +13,22 @@ def about(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+def post_update(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('post-detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_form.html', {'form': form, 'title': 'Modifica Post'})
+
+def post_delete(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        post.delete()
+        return redirect('blog-home')
+    return render(request, 'blog/post_confirm_delete.html', {'post': post})
+
