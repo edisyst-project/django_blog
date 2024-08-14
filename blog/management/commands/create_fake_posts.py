06710862efce5ec4c7142b90a62f8fbb9903fd64
Edoardo <editestar@gmail.com>
2024-08-14 18:56:41 +0200

    Fix faker

--- blog/management/commands/create_fake_posts.py
+++ blog/management/commands/create_fake_posts.py
@@ -1,9 +1,9 @@
 from django.core.management.base import BaseCommand
-from blog.models import create_fake_posts
+from blog.models import create_fake_posts  # Import corretto
 
 class Command(BaseCommand):
     help = 'Crea post fake per testare l\'applicazione'
 
     def handle(self, *args, **kwargs):
-        create_fake_posts(10)
+        create_fake_posts(10)  # Crea 10 post fake
         self.stdout.write(self.style.SUCCESS('10 post fake sono stati creati con successo.'))
--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from faker import Faker
 
+# Modello Post
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
@@ -11,11 +12,12 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
+# Funzione per creare dati fake
+def create_fake_posts(n=5):
     fake = Faker()
-    def create_fake_posts(n=5):
-        for _ in range(n):
-            Post.objects.create(
-                title=fake.sentence(),
-                content=fake.paragraph(),
-                author=fake.name()
-            )
+    for _ in range(n):
+        Post.objects.create(
+            title=fake.sentence(),
+            content=fake.paragraph(),
+            author=fake.name()
+        )
