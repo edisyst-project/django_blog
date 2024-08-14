6e8bf3f69c8cdac1e5ab96e4bf95528b276e34f1
Edoardo <editestar@gmail.com>
2024-08-14 18:51:11 +0200

    Rinomina home.html in post_list.html per maggiore chiarezza

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Post
 
-# Create your tests here.
+class PostModelTest(TestCase):
+    def setUp(self):
+        self.post = Post.objects.create(
+            title="Test Post",
+            content="Test Content",
+            author="Test Author"
+        )
+
+    def test_post_content(self):
+        self.assertEqual(self.post.title, "Test Post")
+        self.assertEqual(self.post.content, "Test Content")
+        self.assertEqual(self.post.author, "Test Author")
+
+class PostDetailViewTest(TestCase):
+    def setUp(self):
+        self.post = Post.objects.create(
+            title="Test Post",
+            content="Test Content",
+            author="Test Author"
+        )
+
+    def test_view_url_exists_at_proper_location(self):
+        response = self.client.get(f'/post/{self.post.pk}/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('post-detail', kwargs={'pk': self.post.pk}))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'blog/post_detail.html')
--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from .models import Post
 
 def home(request):
     posts = Post.objects.all()
-    return render(request, 'blog/home.html', {'posts': posts})
+    return render(request, 'blog/post_list.html', {'posts': posts})
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
